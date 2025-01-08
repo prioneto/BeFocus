783a30270b684f308c7283bb82af0d1cf637e139
dimloldev <wot.mits@gmail.com>
2025-01-08 21:42:20 +0100

    Initial Commit

---
+++ BeFocus/BeFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFocusApp.swift
+//  BeFocus
+//
+//  Created by Dimitris Lolis on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeFocusTests/BeFocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeFocusTests.swift
+//  BeFocusTests
+//
+//  Created by Dimitris Lolis on 08/01/2025.
+//
+
+import Testing
+@testable import BeFocus
+
+struct BeFocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
